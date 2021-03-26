e2ffbc98f57c57c854e786be7e3b7666f7b120cc
Singularity <xhx1247786632@gmail.com>
2021-03-26 10:22:26 +0800

    pool

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import retinex
+from multiprocessing import Pool
 
 config = {
             "sigma_list": [15, 80, 200],##多尺度高斯模糊sigma值
@@ -12,23 +13,38 @@ config = {
             "high_clip" : 0.99
         }
 
-data_dir = '/data/Dataset/DarkFace/image/'
-dst_dir  = '/data/Dataset/DarkFace/Image_MSRCR/'
-
-files = os.listdir(data_dir)
-for file in files:
-    data_path = data_dir + file #'/data/Dataset/DarkFace/image/16.png'
-
-    img = cv2.imread(data_path)
-
-    print('msrcr processing......')
+def MSRCR_Fun(param):
+    data_path, dst_path = param
+    
+    img       = cv2.imread(data_path)
     img_msrcr = retinex.MSRCR(img,config['sigma_list'],config['G'],config['b'],config['alpha'],config['beta'],config['low_clip'],config['high_clip'])
-    cv2.imwrite(dst_dir + file,img_msrcr);
+    cv2.imwrite(dst_path,img_msrcr);
 
-    # print('amsrcr processing......')
     # img_amsrcr = retinex.automatedMSRCR(img,config['sigma_list'])
     # cv2.imwrite('AutomatedMSRCR_retinex.png', img_amsrcr)
 
-    # print('msrcp processing......')
     # img_msrcp = retinex.MSRCP(img, config['sigma_list'], config['low_clip'], config['high_clip'])    
     # cv2.imwrite('MSRCP.png', img_msrcp)
+
+def MSRCR_pool(fps, dfps):
+    pool = Pool(16)
+    pool.map(MSRCR_Fun, zip(fps, dfps))
+    pool.close()
+    pool.join()
+
+if __name__ == '__main__':
+
+    data_dir = '/data/Dataset/DarkFace/image/'
+    dst_dir  = '/data/Dataset/DarkFace/Image_MSRCR/'
+
+    files = os.listdir(data_dir)
+    
+    source_paths = []
+    dst_paths    = []
+    for file in files:
+        source_paths.append(data_dir + file)
+        dst_paths.append(dst_dir + file)
+        
+    MSRCR_pool(source_paths, dst_paths)
+    
+    
